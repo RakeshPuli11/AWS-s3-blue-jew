5e62faff86f1692b475daed7a24a21abfa4d47b6
RakeshPuli11 <pulisairakesh2157@gmail.com>
2024-06-25 14:44:35 +0530

    full stack

--- Backend/routes/router.js
+++ Backend/routes/router.js
@@ -2,7 +2,6 @@ const express = require('express');
 const {  uploadFiles, getAllContent} = require('../controllers/controller.js');
 const router = express.Router();
 
-
 router.post('/singlefileupload', uploadFiles);
 router.get('/getallcontent/:title', getAllContent);
 
